4bde5a675d0a98d71d2aac46d35ef830e2b84f64
James Jones <jamjon3@gmail.com>
2015-11-30 21:46:56 -0500

    More messing

--- lib/CF/Util/Common.pm
+++ lib/CF/Util/Common.pm
@@ -1,12 +1,12 @@
 package CF::Util::Common;
 
 use 5.018002;
-use strict;
-use warnings;
 use namespace::autoclean;
 use YAML::XS 'LoadFile';
 use Data::Dumper;
 use Moose;
+
+our $VERSION = '0.01';
 
 has 'config' => ( is => 'rw', isa => 'HashRef' );
 
